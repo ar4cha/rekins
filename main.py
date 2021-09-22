4b1cb010d35b5c56fbbee4d36b811aef5df7df02
ar4cha <masta2288@gmail.com>
2021-09-22 10:29:39 +0300

    sadsafdas

--- main.py
+++ main.py
@@ -7,8 +7,8 @@ def print_info():
 def lietotaja_ievade():
     print("\033[31m{}\033[0m".format("A bill is proposed legislation under consideration by a legislature.\n "
                                      "A bill does not become law until it is passed by the legislature and, in most cases, approved by the executive."))
-    name = float( input('Your Name: ') )
-    tekst = float(input('Your Tekst:') )
+    name = input('Your Name: ')
+    tekst = input('Your Tekst:')
     size = float(input('how much size box do you want:'))
     height = float(input('How much height does the box want?:'))
     width = float(input('How wide a box do you want? (integers only): '))
@@ -18,9 +18,15 @@ def lietotaja_ievade():
     darba_samaksa = 15
     PVN = 21
 
-    produkta_cena = tekst * 1.2 + width / 100 * length / 100 * height / 100 / 3 * material
+    produkta_cena = width/100.0 * length/100*height/100/3*material
     PVN_summa = produkta_cena + darba_samaksa * PVN / 100
     rekina_summa = produkta_cena + darba_samaksa + PVN_summa
+
+    putin = [str(produkta_cena),str(PVN_summa), str(rekina_summa)]
+
+    f = open("rekins.txt", "w")
+    f.write()
+    f.close()
 
     print("Produkta cena: €" + format(produkta_cena, ",.2f"))
     print("PVN summa: €" + format(PVN_summa, ",.2f"))
